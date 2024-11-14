973068dce4d97f3bc6bd6587926cf9f63a6f89b9
João Francisco Franco Cançado Martins <joao.jffc@gmail.com>
2024-11-14 11:45:52 -0300

    test: Add unit tests

---
+++ src/test/java/com/jffcm/sleeptrack/SleeptrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jffcm.sleeptrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleeptrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
